0f4d65382cddd6c60bd541f6933eaeec532f9eaa
Alex <154066498+raifox@users.noreply.github.com>
2026-05-15 06:08:34 +0300

    Update

---
+++ Coursework C#/Coursework C#/journal.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Coursework_C_
+{
+    partial class journal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtLastName = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtFirstName = new TextBox();
+            txtPatronymic = new TextBox();
+            txtCourse = new TextBox();
+            txtId = new TextBox();
+            txtRating = new TextBox();
+            txtTopic = new TextBox();
+            label8 = new Label();
+            txtPercentage = new TextBox();
+            btnAddStudent = new Button();
+            btnShowHighAchievers = new Button();
+            btnPolymorphismTest = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Прізвище:";
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(147, 20);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(100, 23);
+            txtLastName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Ім'я:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 15);
+            label3.TabIndex = 3;
+            label3.Text = "По батькові:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Курс (1-6):";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(27, 196);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 15);
+            label5.TabIndex = 5;
+            label5.Text = "ІД номер:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(27, 234);
+            label6.Name = "label6";
+            label6.Size = new Size(94, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Рейтинг (0-100):";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(27, 276);
+            label7.Name = "label7";
+            label7.Size = new Size(90, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Тема диплому:";
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(147, 64);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(100, 23);
+            txtFirstName.TabIndex = 8;
+            // 
+            // txtPatronymic
+            // 
+            txtPatronymic.Location = new Point(147, 107);
+            txtPatronymic.Name = "txtPatronymic";
+            txtPatronymic.Size = new Size(100, 23);
+            txtPatronymic.TabIndex = 9;
+            // 
+            // txtCourse
+            // 
+            txtCourse.Location = new Point(147, 149);
+            txtCourse.Name = "txtCourse";
+            txtCourse.Size = new Size(100, 23);
+            txtCourse.TabIndex = 10;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(147, 193);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(100, 23);
+            txtId.TabIndex = 11;
+            // 
+            // txtRating
+            // 
+            txtRating.Location = new Point(147, 231);
+            txtRating.Name = "txtRating";
+            txtRating.Size = new Size(100, 23);
+            txtRating.TabIndex = 12;
+            // 
+            // txtTopic
+            // 
+            txtTopic.Location = new Point(147, 273);
+            txtTopic.Name = "txtTopic";
+            txtTopic.Size = new Size(100, 23);
+            txtTopic.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(27, 318);
+            label8.Name = "label8";
+            label8.Size = new Size(97, 15);
+            label8.TabIndex = 14;
+            label8.Text = "Відсоток (0-100):";
+            // 
+            // txtPercentage
+            // 
+            txtPercentage.Location = new Point(147, 315);
+            txtPercentage.Name = "txtPercentage";
+            txtPercentage.Size = new Size(100, 23);
+            txtPercentage.TabIndex = 15;
+            // 
+            // btnAddStudent
+            // 
+            btnAddStudent.Location = new Point(27, 388);
+            btnAddStudent.Name = "btnAddStudent";
+            btnAddStudent.Size = new Size(114, 23);
+            btnAddStudent.TabIndex = 16;
+            btnAddStudent.Text = "Додати студента";
+            btnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnShowHighAchievers
+            // 
+            btnShowHighAchievers.Location = new Point(178, 388);
+            btnShowHighAchievers.Name = "btnShowHighAchievers";
+            btnShowHighAchievers.Size = new Size(134, 23);
+            btnShowHighAchievers.TabIndex = 17;
+            btnShowHighAchievers.Text = "Показати відмінників";
+            btnShowHighAchievers.UseVisualStyleBackColor = true;
+            // 
+            // btnPolymorphismTest
+            // 
+            btnPolymorphismTest.Location = new Point(355, 388);
+            btnPolymorphismTest.Name = "btnPolymorphismTest";
+            btnPolymorphismTest.Size = new Size(157, 23);
+            btnPolymorphismTest.TabIndex = 18;
+            btnPolymorphismTest.Text = "Тест пізнього зв'язування";
+            btnPolymorphismTest.UseVisualStyleBackColor = true;
+            // 
+            // journal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(546, 450);
+            Controls.Add(btnPolymorphismTest);
+            Controls.Add(btnShowHighAchievers);
+            Controls.Add(btnAddStudent);
+            Controls.Add(txtPercentage);
+            Controls.Add(label8);
+            Controls.Add(txtTopic);
+            Controls.Add(txtRating);
+            Controls.Add(txtId);
+            Controls.Add(txtCourse);
+            Controls.Add(txtPatronymic);
+            Controls.Add(txtFirstName);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtLastName);
+            Controls.Add(label1);
+            Name = "journal";
+            Text = "Журнал";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtLastName;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtFirstName;
+        private TextBox txtPatronymic;
+        private TextBox txtCourse;
+        private TextBox txtId;
+        private TextBox txtRating;
+        private TextBox txtTopic;
+        private Label label8;
+        private TextBox txtPercentage;
+        private Button btnAddStudent;
+        private Button btnShowHighAchievers;
+        private Button btnPolymorphismTest;
+    }
+}
---
+++ Coursework C#/Coursework C#/journal.cs
@@ -0,0 +1,10 @@
+namespace Coursework_C_
+{
+    public partial class journal : Form
+    {
+        public journal()
+        {
+            InitializeComponent();
+        }
+    }
+}
